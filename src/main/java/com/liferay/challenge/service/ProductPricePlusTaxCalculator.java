fe4e74534e27aad6b8b3cba8761991f144426626
brunoquei2105 <bruno.queiroz@liferay.com>
2021-09-08 16:45:57 -0300

    SF

--- src/main/java/com/liferay/challenge/service/ProductPricePlusTaxCalculator.java
+++ src/main/java/com/liferay/challenge/service/ProductPricePlusTaxCalculator.java
@@ -3,22 +3,23 @@ package com.liferay.challenge.service;
 
 import com.liferay.challenge.service.Constants.ProductTaxConstant;
 
+
 public class ProductPricePlusTaxCalculator {
 
 
     public static double pricePlusBasicTax(double price){
-       return price* ProductTaxConstant.PRICE_PLUS_BASIC_TAX;
+        return price* ProductTaxConstant.PRICE_PLUS_BASIC_TAX;
     }
 
-   public static double pricePlusImportedTax(double price){
+    public static double pricePlusImportedTax(double price){
         return price * ProductTaxConstant.PRICE_PLUS_IMPORTED_TAX;
    }
 
-   public static double pricePlusImportedBasicTax(double price){
+    public static double pricePlusImportedBasicTax(double price){
         return price * ProductTaxConstant.PRICE_PLUS_IMPORTED_AND_BASIC_TAX;
    }
 
-   public static double priceWithFreeTax(double price){
+    public static double priceWithFreeTax(double price){
         return price;
    }
 
